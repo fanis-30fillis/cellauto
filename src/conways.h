f98b943ff03320308db4e4f53b80faab87f2c188
Fanis Triantafyllis <fanis_30fillis@outlook.com>
2026-08-22 14:38:00 +0300

    Fixed inconsistency

--- src/conways.h
+++ src/conways.h
@@ -8,8 +8,8 @@ struct cell {
 	bool nextState;
 };
 
-void random_seed(struct cell** res, size_t rows, size_t cols, double aliveChance);
-void clearCells(struct cell** res, size_t rows, size_t cols);
-uint8_t get_live_neighbors(struct cell** res, size_t rows, size_t cols, int current_row, int current_col);
-bool set_next_state(struct cell** res, size_t rows, size_t cols);
-void calculate_next_array_state(struct cell** res, size_t rows, size_t cols);
+void random_seed(struct cell* res, size_t rows, size_t cols, double aliveChance);
+void clearCells(struct cell* res, size_t rows, size_t cols);
+uint8_t get_live_neighbors(struct cell* res, size_t rows, size_t cols, int current_row, int current_col);
+bool set_next_state(struct cell* res, size_t rows, size_t cols);
+void calculate_next_array_state(struct cell* res, size_t rows, size_t cols);
