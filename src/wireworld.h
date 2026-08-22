5f74b681730ef033a13ec78b9f5f26fb34d2c4df
Fanis Triantafyllis <fanis_30fillis@outlook.com>
2026-08-22 12:49:56 +0300

    Begun wireworld

---
+++ src/wireworld.h
@@ -0,0 +1,14 @@
+enum WireworldCellState {
+    EMPTY,
+    CONDUCTOR,
+    HEAD,
+    TAIL
+};
+
+struct WireworldCell {
+    enum WireworldCellState state;
+    enum WireworldCellState nextState;
+};
+
+void changeStatusOfCellInPosition(struct WireworldCell *grid, int rows, int cols, int mouseRow, int mouseCol);
+void calculateNextStatus(struct WireworldCell* grid, int rows, int cols);
